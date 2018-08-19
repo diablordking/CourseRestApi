365f2e82e251b8cd062286ec944a259dee6d329c
diabloking <diablordking@gmail.com>
2018-08-19 18:50:09 +0300

    Update endpoint

--- src/main/java/io/alp/CourseApiDataApplication.java
+++ src/main/java/io/alp/CourseApiDataApplication.java
@@ -1,11 +1,11 @@
-package io.javabrains;
+package io.alp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
-@EnableJpaRepositories("io.javabrains")
+@EnableJpaRepositories("io.alp")
 public class CourseApiDataApplication {
 
 	public static void main(String[] args) {
